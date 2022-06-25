82df510571a06d95b0d42c976c9f6c79b5b62928
Jimmy Solis <jimmysolis14@gmail.com>
2022-06-25 12:33:56 -0400

    addede the enter button for moblie, and made the redirect replace through hyperlink.

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,27 @@
 var enterLog = document.body
+var blink = document.getElementById('blink');
+
+
+setInterval(function () {
+
+    blink.style.opacity = 
+
+    (blink.style.opacity == 0 ? 1 : 0);
+
+}, 500); 
 
 
 enterLog.addEventListener("keydown",keyPress);
 
 function keyPress(event){
-
     const keyName = event.key;
-    
     if (keyName === 'Enter') {
-
       handelRedirect();
       console.log("redirected")
-
       }
 }
   
 
 function handelRedirect(){
     location.assign('homePage.html');
-
-
 }
